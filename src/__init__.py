dc22a4926b595d807a6afc4e1f015a2a4c49d1a5
Caio J. Carvalho <cjlcarvalho@live.com>
2017-08-08 00:12:18 -0300

    Stock market prediction app

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from src.configs import settings
 
 app = Flask(__name__)
 
--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,9 @@
+from flask import Flask, render_template
 from src.configs import settings
-from __init__ import app
+
+app = Flask(__name__)
+
+from src.routes import routes
 
 if __name__ == '__main__':
     app.run(port=settings.PORT, host=settings.HOST)
